f70ab3cdf8209b6fa6cba7ed6a9bbfb10b4feb49
Andrew Song <andrewgnos@gmail.com>
2017-07-08 23:39:20 -0700

    remove dist

--- dist/src/Types/Types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var typeDefs = "\n  type User {\n    id: Int!\n    firstName: String\n    lastName: String\n  }\n\n  #Query\n  type Query {\n    users: [User]\n    user(firstName: String): User\n  }\n\n  #Mutation\n";
-
-exports.default = typeDefs;
-module.exports = exports["default"];
